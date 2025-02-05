b18c0113e28f882035185c4f67ae0fa8911bbd39
simeiro <shitimi1633@gmail.com>
2025-02-06 01:32:53 +0900

    修正：ダーク適用されていない部分の修正

--- app/page.tsx
+++ app/page.tsx
@@ -102,13 +102,13 @@ export default function Home() {
           <h2 className="text-2xl">よくある質問</h2>
           <hr className="mb-4"/>
           <div className="max-w-xl mx-auto mt-8 space-y-4">
-              <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+              <div className="bg-gray-100 p-4 rounded-lg shadow-md dark:text-black">
                   <p className="font-bold text-lg">Q. 明らかに背景でないものが問題に出てくる...</p>
                   <p className="mt-1 text-gray-700">
                       A. 背景の色はプログラムによって自動検出しているため、間違いが生じる場合があります。
                   </p>
               </div>
-              <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+              <div className="bg-gray-100 p-4 rounded-lg shadow-md dark:text-blac">
                   <p className="font-bold text-lg">Q. あの有名な曲が問題に出てこない</p>
                   <p className="mt-1 text-gray-700">
                       A. プログラムや問題の都合上、背景色が明らかでない背景、色味の少ない背景は除外しています。また、誤って除外されているケースもあります。
